36972655ca01ce38ea34e456c0195d006cc7189b
ColinFransman <colinfransman1@gmail.com>
2025-10-08 11:37:36 +0200

    feat: created test import

---
+++ app/Imports/TestImport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Test;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class TestImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Test([
+            'test' => $row[0],
+            'data' => $row[1],
+        ]);
+    }
+}
