2cd74d48d2037031a5d4055f48bbebb69010acf8
Jesper <jesper@nedfinity.com>
2025-11-28 12:21:56 +0100

    feat: published language files, added dutch lang files

---
+++ lang/nl/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Uw wachtwoord is gereset.',
+    'sent' => 'We hebben uw wachtwoordresetlink per e-mail verzonden.',
+    'throttled' => 'Wacht even voordat u het opnieuw probeert.',
+    'token' => 'Deze wachtwoordresettoken is ongeldig.',
+    'user' => 'We kunnen geen gebruiker met dat e-mailadres vinden.',
+];
