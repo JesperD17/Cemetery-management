118967700f49a791d167903f47f373a4463dcf1d
idontknow83 <Manoncristel37@gmail.com>
2025-12-14 22:18:37 +0100

    fixed: comments 1/2

--- resources/js/pages/includes/Header.svelte
+++ resources/js/pages/includes/Header.svelte
@@ -7,24 +7,19 @@
   }
 
   let { user: propUser }: Props = $props();
-
   const derivedUser = $derived($page?.props?.auth?.user || null);
-  const userRole = $derived($page?.props?.auth?.user?.role?.name || null);
 </script>
 
 <div class="padding-all row-flex align-center justify-between border-primary-btm bg-primary">
   <div class="h2">DNBP</div>
 
   <div class="flex-m-gap align-center">
-
     <NotificationsDropdown />
 
     <Link href={route('home')}>Home</Link>
 
     {#if propUser ?? derivedUser}
-      {#if userRole !== 'rechthebbende'}
-        <Link href={route('profiel')}>Profiel</Link>
-      {/if}
+      <Link href={route('profiel')}>Profiel</Link>
 
       <Link
         class="btn primary"
